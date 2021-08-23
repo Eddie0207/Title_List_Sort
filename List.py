0666c325b8125632d73d297db6e8929d2c79a10a
Eddie0207 <adeemshahzad@gmail.com>
2021-08-23 14:49:27 -0500

    Update List.py

--- List.py
+++ List.py
@@ -1,6 +1,6 @@
 bicycles=['trek' , 'cannondale' , 'redline' , 'specialized']
 friends =['Jack' , 'wade' , 'rate']
-message_1= " My first bicycle was a " + bicycles [-2].title()
+message_1= f"My first bicycle was a {bicycles [-2].title()}"
 message_2 = f"My best friend {friends [2].title()} had {bicycles[-1]} bicycle"
 print (message_1)
 print (message_2)
