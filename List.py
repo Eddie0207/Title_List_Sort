00766845f61bfa7ce9b24e0b0306dff545371c68
Eddie0207 <adeemshahzad@gmail.com>
2022-07-29 14:24:55 -0500

    updated
    updated messages

--- List.py
+++ List.py
@@ -1,6 +1,17 @@
-bicycles=['trek' , 'cannondale' , 'redline' , 'specialized']
-friends =['Jack' , 'wade' , 'rate']
-message_1= f"My first bicycle was a {bicycles[-2].title()}"
-message_2 = f"My best friend {friends [2].title()} had {bicycles[-1]} bicycle"
-print (message_1)
-print (message_2)
+bicycles = ['Trek' , 'Cannondale' , 'Redline' , 'Specialized']
+buddies = ['Jack' , 'wade' , 'Bethany']
+buddies.insert(2, 'Willow')
+buddies.sort()
+print (buddies)
+print (f"My first bicycle was a {bicycles[3].title()}")
+print (f"My best friend {buddies [3].title()} had {bicycles[0]} bicycle")
+print (f"There are total {len(bicycles)} bicycles")
+
+
+
+
+
+
+
+
+3
