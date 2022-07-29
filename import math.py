c040131d08ecef0602d1d99c213e85446e2aac74
Eddie0207 <adeemshahzad@gmail.com>
2022-07-29 14:31:43 -0500

    new
    updated

---
+++ import math.py
@@ -0,0 +1,18 @@
+import math
+x = math.sqrt(100)
+print (x)
+y = math.factorial(8)
+print (y)
+if x > 10:
+ 	print ("X is not greater than 10")
+else:
+ 	print("X is either equal or smaller than 10")
+if y < 20000:
+	print ("Y is smaller")
+elif y > 500000:
+	print("Y is greater than 500000")
+else:
+	print ("Y is between 20000 and 500000")
+while x != 0:
+	x -= 1
+
---
+++ pizzas.py
@@ -0,0 +1,5 @@
+pizzas = ['Pepperoni' , 'Chicken' , 'Veggie']
+for pizza in pizzas:
+	print (f"I like {pizza.title()} pizza")
+	print (f"when it\'s Hot {pizza.title()} pizza\n")
+print (f"I really HOT & CHEESEy love Pizza")
