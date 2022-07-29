8c0fc03e35d23580724bd6676313ff877a1e8ba0
Eddie0207 <adeemshahzad@gmail.com>
2022-07-28 21:01:28 -0500

    Add files via upload

---
+++ New Bycycle list.py
@@ -0,0 +1,14 @@
+bicycles=['Trek' , 'Cannondale' , 'Redline' , 'Specialized']
+for bicycle in bicycles:
+	print(bicycle)
+friends=['Jack' , 'wade' , 'Bethany']
+friends.insert(2, 'Willow')
+friends.sort()
+for friend in friends:
+	print (friend)
+message_1= f"My first bicycle was a {bicycles[3].title()}"
+message_2 = f"My best friend {friends [3].title()} had {bicycles[0]} bicycle"
+print (message_1)
+print (message_2)
+
+
---
+++ nth_root.py
@@ -0,0 +1,22 @@
+def nth_root(radicand, n):
+	return radicand ** (1/n)
+
+def ordinal_suffix(value):
+	s=str(value)
+	if s.endswith('1'):
+		return'st'
+	elif s.endswith('2'):
+		return'nd'
+	elif s.endswith('3'):
+		return'rd'
+	return'th'
+
+def ordinal(value):
+	return str(value) + ordinal_suffix(value)
+
+def display_nth_root(radicand, n):
+	root = nth_root(radicand, n)
+	message = "the " + ordinal(n) + " Root Of "  + str(radicand) + " is " + str(root)
+	print (message)
+
+display_nth_root(27, 3)
